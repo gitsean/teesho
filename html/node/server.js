d0bc962cfc8fc46ea26e0922382261fd9242979b
gitSean <sareid@outlook.com>
2017-03-19 18:02:52 -0400

    saving to mysql

--- html/node/server.js
+++ html/node/server.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'Shug1276',
-  database : 'my_db'
+  database : 'ticker_tape'
 });
 
 
@@ -15,14 +15,60 @@ app.get('/saySomething', function (req, res) {
    res.end( 'Ok' );
 });
 
-var j = schedule.scheduleJob('*/1 * * * *', function(){
-  requestify.get('https://api.coinmarketcap.com/v1/ticker/').then(function(response) {
+var rule = new schedule.RecurrenceRule();
+
+rule.minute = 00;
+
+var j = schedule.scheduleJob(rule, function(){
+
+
+
+  	requestify.get('https://api.coinmarketcap.com/v1/ticker/').then(function(response) {
     // Get the response body
+
+    	console.log('back')
+
 	    var ticker = response.getBody();
-	    console.log(ticker);
+
+	    connection.connect(function(err) {
+			if (err) {
+				return;
+			}
+			var values = []
+			ticker.forEach(function(item){
+		    	item.tid = item.id;
+		    	delete item.id;
+		    	// var littleArray = [];
+		    	// for(k in Object.keys(item)){
+		    	// 	littleArray.push(item[k]);
+		    	// }
+		    	var littleArray = Object.keys(item).map(function(key) {
+				    return item[key];
+				});
+		    	values.push(littleArray);
+		    });
+		    
+		    var keys = "(" + Object.keys(ticker[0]).join() + ")";
+	    	var sql = "INSERT INTO tickers " + keys + " VALUES ?";
+		    
+		    console.log(values[22]);
+
+			var query = connection.query(sql, [values], function (error) {
+			  if (error) throw error;
+			  // Neat!
+			  console.log('success');
+			  connection.end();
+			});
+
+		  	
+		});
+	}, function(e){
+		console.log(e)
 	});
 
 });
+
+
 
 var server = app.listen(8081, function () {
 
