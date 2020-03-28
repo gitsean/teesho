6652e72abfc165a6ed11c42e1c880e1245713947
Sean <sareid@outlook.com>
2020-03-28 14:59:59 -0400

    Begin to add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from "react";
-import "./App.css";
+// BUILDING AUTH:
+// https://medium.com/better-programming/building-basic-react-authentication-e20a574d5e71
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import PrivateRoute from "./PrivateRoute";
+import { AuthContext } from "./context/auth";
+
+class App extends React.Component<{}> {
+  render() {
+    return (
+      <AuthContext.Provider value={false}>
+        <Router>
+          <div>
+            <ul>...</ul>
+            <Route exact path="/" component={Home} />
+            <PrivateRoute path="/admin" component={Admin} />
+          </div>
+        </Router>
+      </AuthContext.Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/PrivateRoute.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "./context/auth";
+
+function PrivateRoute({
+  component: Component,
+  ...rest
+}: {
+  component: any;
+  path: any;
+}) {
+  const isAuthenticated = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
