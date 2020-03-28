202021a55abf71c6487861dc805244b8ad7d09c2
Sean <sareid@outlook.com>
2020-03-28 12:54:26 -0400

    Making something

---
+++ html/precache-manifest.2b4b8fb79c9b8a03a5c495ca7d8e4c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f066bfeac2b68bd082ec41bf400376aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e5a5d64feedeee01d49",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fcc66a73fd4ae590015",
+    "url": "/static/js/2.81a66362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.81a66362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e5a5d64feedeee01d49",
+    "url": "/static/js/main.4b843818.chunk.js"
+  },
+  {
+    "revision": "806fe0006227a7ef94de",
+    "url": "/static/js/runtime-main.e4c3cae1.js"
+  }
+]);
